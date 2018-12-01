71fdd7c59703e1f758c883dafe73542636b784f5
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-12-01 21:38:16 +0530

    fix-dataset path

--- src/train.py
+++ src/train.py
@@ -1,5 +1,5 @@
 from sklearn.datasets import load_files
 
-data = load_files('./dataset/txt_sentoken', encoding="utf-8", decode_error="replace")
+data = load_files('../dataset/txt_sentoken', encoding="utf-8", decode_error="replace")
 
 print(type(data))
