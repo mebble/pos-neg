9f9a0048e2d23c70bed00ecf856704010d02d3a7
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2018-12-23 21:53:32 +0530

    pickle the trained model

--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+import pickle
 from sklearn.datasets import load_files
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -8,6 +9,8 @@ from sklearn.metrics import accuracy_score
 data = load_files('../dataset/txt_sentoken', encoding="utf-8", decode_error="replace")
 x_train, x_test, y_train, y_test = train_test_split(data.data, data.target)
 labels = [label for label in  enumerate(data.target_names)]
+
+print('Computing...')
 
 # Use a TFIDF vectorizer with a vocabulary of the top 3000 most frequent words
 vectorizer = TfidfVectorizer(stop_words="english", max_features=3000, decode_error="ignore")
@@ -22,4 +25,12 @@ model.fit(x_train_vec, y_train)
 # Test the model
 predictions = model.predict(x_test_vec)
 accuracy = accuracy_score(y_test, predictions)
-print(accuracy)
+print("The model's accuracy is {0:.2f}%".format(100 * accuracy))
+
+print('Pickling the model and the vectorizer...')
+with open('model.pickle', 'wb') as fd:
+    pickle.dump(model, fd)
+with open('vectorizer.pickle', 'wb') as fd:
+    pickle.dump(vectorizer, fd)
+
+print('Trained and saved the model and vectorizer.')
